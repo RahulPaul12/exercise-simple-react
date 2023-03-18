79938ddf21bb8a4024b0c56fb52a13c209563c66
Rahul Paul <rahulpaul199914@gmail.com>
2023-03-18 12:55:49 +0600

    Add files via upload

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -5,30 +5,40 @@ import './Main.css'
 const Main = () => {
     const [product, setProduct] = useState([]);
     const [count, setCount] = useState(0)
-    
+    const [search, setSearch] = useState([])
+
+    const handleSearch =(e)=>{
+       
+        const searchResult = product.filter(product => product.first_name.toLowerCase().includes(e.target.value.toLowerCase()))
+        setSearch(searchResult)
+    }
     
     const handleAddTime = (second) => {
-        
             setCount(count + parseInt(second))
-            
-            
-            
+   
         }
     useEffect(()=>{
         fetch('./fakedata.json')
         .then(res=> res.json())
-        .then(data => setProduct(data))
+        .then(data => {
+            setProduct(data)
+            setSearch(data)})
     },[])
     return (
         <div className='main'>
             <div className="product">
                 <div className="header-text">
-                    <h2>ULTRA-ACTIVE-CLUB</h2>
-                     <p>Select todays exercise</p>
+                    <div>
+                        <h2>ULTRA-ACTIVE-CLUB</h2>
+                        <p>Select todays exercise</p>
                      </div>
+                        <div className="search">
+                            <input type="text" placeholder="Search item" onChange={handleSearch}/>
+                     </div>
+                </div>
                      <div className="all-product">
                         {
-                        product.map(product => <Product product={product}  handleAddTime={handleAddTime} ></Product>)
+                        search.map(product => <Product product={product}  handleAddTime={handleAddTime} ></Product>)
                      }
                      </div>
                      
